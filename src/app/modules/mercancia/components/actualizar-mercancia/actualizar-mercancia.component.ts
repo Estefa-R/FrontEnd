18215a9d89513d213fe963f05f368cd4b7946f78
Estefany Rodriguez Bedoya <erodriguez1193@gmail.com>
2022-10-26 11:42:45 -0500

    Formato de fecha

--- src/app/modules/mercancia/components/actualizar-mercancia/actualizar-mercancia.component.ts
+++ src/app/modules/mercancia/components/actualizar-mercancia/actualizar-mercancia.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MercanciaService } from 'src/app/modules/shared/services/mercancia.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmpleadoService } from 'src/app/modules/shared/services/empleado.service';
 
 @Component({
   selector: 'app-actualizar-mercancia',
@@ -10,13 +11,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ActualizarMercanciaComponent implements OnInit {
   mercancia: any;
+  empleados: any;
 
   public actualizarForm: FormGroup;
   estadoFormulario: string = "";
   panelOpenState = false;
 
+  fecha_ingreso: any;
+
   constructor(private fb: FormBuilder,
     private mercanciaService: MercanciaService,
+    private empleadoService: EmpleadoService,
     private dialogRef: MatDialogRef<ActualizarMercanciaComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
       this.estadoFormulario = "Actualizar";
@@ -26,8 +31,16 @@ export class ActualizarMercanciaComponent implements OnInit {
         nombre: [data.nombre, Validators.required],
         cantidad: [data.cantidad, Validators.required],
         id_empleado: [data.id_empleado, Validators.required],
-        fecha_modificacion: [data.fecha_modificacion, Validators.required]
+        fecha_modificacion: [data.fecha_modificacion, Validators.required],
       });
+      this.fecha_ingreso = data.fecha_ingreso
+
+
+
+    // hacemos getAllEmpleados y lo ponemos en una variable que nos almacena los datos de los empleados
+    this.empleadoService.getAllEmpleado().subscribe( (dataMercancia:any) => {
+      this.empleados = dataMercancia;
+    });
     }
 
     ngOnInit(): void {
@@ -39,8 +52,9 @@ export class ActualizarMercanciaComponent implements OnInit {
       nombre: this.actualizarForm.get('nombre')?.value,
       cantidad: this.actualizarForm.get('cantidad')?.value,
       id_empleado: this.actualizarForm.get('id_empleado')?.value,
-      fecha_modificacion:  this.actualizarForm.get('fecha_modificacion')?.value
-    }
+      fecha_ingreso: this.fecha_ingreso,
+      fecha_modificacion: new Date
+        }
 
     this.mercanciaService.updateMercancia(mercanciaFormData).subscribe((mercanciaFormData: any)=>{
         this.dialogRef.close(1);
